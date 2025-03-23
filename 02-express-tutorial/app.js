1c948795e623e8a1ebc1a59ec04bd1a36d30586c
nihalerdal <nihalerdall@gmail.com>
2025-03-22 17:47:28 -0700

    search by product id

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,34 @@
-console.log('Express Tutorial')
+console.log("Express Tutorial");
+
+const express = require("express");
+const app = express();
+const { products } = require("./data");
+
+app.use(express.static("./public"));
+
+app.get("/api/v1/products", (req, res) => {
+  res.json(products);
+});
+
+app.get("/api/v1/products/:productID", (req, res) => {
+  const idToFind = parseInt(req.params.productID); // convert productID to number
+  const product = products.find((p) => p.id === idToFind); //find the product
+
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404).json({ message: "That product was not found." });
+  }
+});
+
+app.post("/", (req, res) => {
+  res.status(200).send("Received a POST request.");
+});
+
+app.all("*", (req, res) => {
+  res.status(404).send("resource not found");
+});
+
+app.listen(3000, (req, res) => {
+  console.log("server is listening on port 3000");
+});
