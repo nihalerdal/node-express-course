131320210811632a2df8b6e8ec97d0cb67e13184
nihalerdal <nihalerdall@gmail.com>
2025-04-08 14:33:05 -0700

    Refactor with asyncWrapper and custum error handling

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -2,18 +2,21 @@ const express = require("express");
 const app = express();
 const tasks = require("./routes/tasks");
 const connectDB = require("./db/connect");
-const notFound = require('./middleware/not-found')
+const notFound = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+const asyncWrapper = require("./middleware/async");
 require("dotenv").config();
 
 //middleware
 app.use(express.json());
-app.use(express.static('./public'))
+app.use(express.static("./public"));
 
 //routes
 app.use("/api/v1/tasks", tasks);
-app.use(notFound)
 
-const port = 3000;
+app.use(notFound);
+app.use(errorHandlerMiddleware);
+const port = process.env.port || 3000;
 
 const start = async () => {
   try {
