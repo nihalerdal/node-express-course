ba459685f243837aacc3e01be3dbbafbda0d02e2
nihalerdal <nihalerdall@gmail.com>
2025-04-15 15:58:02 -0700

    Regex query parameter

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -6,17 +6,17 @@ const getAllProductsStatic = async (req, res) => {
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company , name} = req.query;
+  const { featured, company, name } = req.query;
   const queryObject = {};
   if (featured) {
     queryObject.featured = featured === "true" ? true : false;
   }
-   if (company) {
-     queryObject.company = company
-   }
-   if (name) {
-     queryObject.name = name;
-   }
+  if (company) {
+    queryObject.company = company;
+  }
+  if (name) {
+    queryObject.name = { $regex: name, $options: i };
+  }
   console.log(queryObject);
   const products = await Product.find(req.query);
   res.status(200).json({ products, nbHits: products.length });
